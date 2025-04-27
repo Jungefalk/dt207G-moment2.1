225707ff66b6980d18981b86f86dd3ff848532dc
Caroline <c.jungefalk@gmail.com>
2025-04-27 21:49:11 +0200

    Hänta databastabell med query, samt kontroll med statuskod vid fel

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ connection.connect((error) => {
     if (error) {
         console.log(`Connection failed ${error}`)
     }
-    
+
     console.log(`Connected to database`)
 
 });
@@ -49,11 +49,29 @@ app.get("/api", (req, res) => {
 
 //read
 app.get("/api/work_experience", (req, res) => {
-    res.json({ message: "Get work experience" })
+
+    //Hämta databastabell
+    connection.query(`SELECT * FROM work_experience`, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: `Something went wrong ${err}` });
+            return;
+        }
+
+        console.table(results);
+        //kontrollera att det finns data att hämta
+        if (results.length === 0) {
+            res.status(200).json({ message: "No work experience found" })
+        } else {
+            res.json(results);
+        }
+
+    })
+
 });
 
 //create
 app.post("/api/work_experience", (req, res) => {
+
     res.json({ message: "Work expreience added" })
 });
 
