9bfbe6cf167a6546c247433f543d95b073ea1283
Caroline <c.jungefalk@gmail.com>
2025-04-27 20:14:50 +0200

    lagt till databasanslutning med mysql

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
+//databasanslutning med mysql
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+//Kontrollera anslutning
+connection.connect((error) => {
+    if (error) {
+        console.log(`Connection failed ${error}`)
+    }
+
+    console.log(`Connected to database`)
+
+});
+
 //middleware
 app.use(express.json());
 app.use(cors());
