8b39212b060fba5503f8d6ca6add1742c4409aef
Caroline <c.jungefalk@gmail.com>
2025-04-30 12:25:41 +0200

    Gjort om installscript för att använda sqlite istället för mysql

--- install.js
+++ install.js
@@ -1,65 +1,73 @@
 /**
- * Anslut till- och skapa mysql databas med tabell och värden
+ * Anslut till- och skapa sqlite3 databas med tabell och värden
  */
 
 //Importera paket
-const mysql = require("mysql");
-require("dotenv").config();
+const sqlite3 = require("sqlite3").verbose();
 
-//Databasanslutnig
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
-//Kontrollera anslutning
-connection.connect((error) => {
+//Skapa databas och kontrollera anslutning
+const db = new sqlite3.Database("./db/cv.db", (error) => {
     if (error) {
-        console.log(`Connection failed ${error}`)
+        return console.error("Connection to database failed " + error.message);
     }
-
-    console.log(`Connected to database`)
+    console.log("Connected to database");
 
 });
 
-//Query för att radera tabellen om den redan existerar
-connection.query("DROP TABLE IF EXISTS work_experience", (error, result) => {
-    if (error) {
-        throw error;
-    }
-    console.log("Table dropped")
-})
+//Skapa tabell till databas, radera om den redan finns
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS work_experience", (error) => {
+        if (error) {
+            throw error;
+        }
+        console.log("table dropped")
+    });
 
-//Query för att skapa tabellen work_experience
-connection.query(`CREATE TABLE work_experience (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    company_name VARCHAR (100),
-    job_title VARCHAR (100),
-    location VARCHAR (100),
+    //Skapa tabell
+    db.run(`CREATE TABLE work_experience (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT NOT NULL,
+    job_title TEXT NOT NULL,
+    location TEXT NOT NULL,
     start_date DATE,
     end_date DATE,
     description TEXT
-);`, (error, result) => {
-    if (error) {
-        throw error;
-    }
-    console.log("work_experience table created")
-});
+    );`, (error) => {
+        if (error) {
+            throw error;
+        };
 
-//Skapa data till tabellen
-connection.query("INSERT INTO work_experience (company_name, job_title, location, start_date, end_date, description)VALUES(?, ?, ?, ?, ?, ?)", ["Rusta", "Butiksanställd", "Bromma", "2019-10-10", "2021-03-31", "Kundservice. Kassa. Påfyll. Lager. Inventering."], (error, result) => {
-    if (error) {
-        throw error;
-    }
-    console.log("Work experience added")
-});
+        console.log("Table created");
 
-connection.query("INSERT INTO work_experience (company_name, job_title, location, start_date, end_date, description)VALUES(?, ?, ?, ?, ?, ?)", ["Sekelskifte", "Lagerarbetare", "Västberga", "2021-04-16", "2025-09-01", "Inleverans. Truckkörning. Returhantering. Inventering. Plocka och packa."], (error, result) => {
-    if (error) {
-        throw error;
-    }
-    console.log("Work experience added")
-});
+        //Skapa data till tabell
+        const stmt = db.prepare("INSERT INTO work_experience (company_name, job_title, location, start_date, end_date, description)VALUES(?, ?, ?, ?, ?, ?);");
 
+        stmt.run("Rusta", "Butiksanställd", "Bromma", "2019-10-10", "2021-03-31", "Kundservice. Kassa. Påfyll. Lager. Inventering.", (error, result) => {
+            if (error) {
+                throw error;
+            };
+            console.log("Work experience added");
+        });
+
+        stmt.run("Sekelskifte", "Lagerarbetare", "Västberga", "2021-04-16", "2025-09-01", "Inleverans. Truckkörning. Returhantering. Inventering. Plocka och packa.", (error, result) => {
+            if (error) {
+                throw error;
+            }
+            console.log("Work experience added");
+        });
+
+        stmt.finalize((error) => {
+            if (error) {
+                throw error;
+            }
+
+            //Stäng databasen
+            db.close((error) => {
+                if (error) {
+                    throw error;
+                }
+                console.log("database closed");
+            });
+        });
+    });
+});
